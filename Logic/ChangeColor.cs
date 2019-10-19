bf868114a7c7142a43f2eaf102fe2e9046cf8b39
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2019-10-19 15:26:24 +0300

    first_commit

---
+++ Logic/ChangeColor.cs
@@ -0,0 +1,55 @@
+﻿namespace Logic
+{
+    public class ChangeColor
+    {
+        public static MyEnum C;
+        public static MyEnum2 C2;
+        public enum MyEnum2
+        {
+            Black,
+            Yellow
+        }
+        public enum MyEnum
+        {
+            Red,
+            RedAndYellow,
+            Yellow,
+            Green,
+        }
+        public static void ChangeLight2(object sender, object e)
+        {
+            switch (C2)
+            {
+                case MyEnum2.Yellow:
+                    C2 = MyEnum2.Black;
+                    break;
+                case MyEnum2.Black:
+                    C2 = MyEnum2.Yellow;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public static void ChangeLight(object sender, object e)
+        {
+            switch (C)
+            {
+                case MyEnum.Red:
+                    C = MyEnum.RedAndYellow;
+                    break;
+                case MyEnum.RedAndYellow:
+                    C = MyEnum.Green;
+                    break;
+                case MyEnum.Green:
+                    C = MyEnum.Yellow;
+                    break;
+                case MyEnum.Yellow:
+                    C = MyEnum.Red;
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
+}
---
+++ traffic light/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Logic;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using static Logic.ChangeColor;
+
+
+namespace traffic_light
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        DispatcherTimer timer2 = new DispatcherTimer();
+        static int count = 0;
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+                //int h = DateTime.Now.Hour;
+                //int m = DateTime.Now.Minute;
+                //int s = DateTime.Now.Second;
+
+                timer.Tick += ChangeColor.ChangeLight;
+                timer.Tick += Change;
+                timer.Tick += NOW;
+
+                timer2.Tick += NOW;
+                timer2.Tick += ChangeLight2;
+                timer2.Tick += Change;
+
+                ChangeColor.C = MyEnum.Yellow;
+
+                timer.Start();
+
+                void NOW(object sender, object e)
+                {
+                    count++;
+                }
+                void Change(object sender, object e)
+                {
+                    if (count > 10)
+                    {
+                        timer2.Start();
+                        switch (ChangeColor.C2)
+                        {
+                            case MyEnum2.Black:
+                                timer2.Interval = new TimeSpan(0, 0, 0, 1);
+                                RedL.Fill = Brushes.Black;
+                                YellowL.Fill = Brushes.Black;
+                                GreenL.Fill = Brushes.Black;
+                                break;
+                            case MyEnum2.Yellow:
+                                timer2.Interval = new TimeSpan(0, 0, 0, 1);
+                                RedL.Fill = Brushes.Black;
+                                YellowL.Fill = Brushes.Yellow;
+                                GreenL.Fill = Brushes.Black;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        switch (ChangeColor.C)
+                        {
+                            case MyEnum.Red:
+                                timer.Interval = new TimeSpan(0, 0, 0, 3);
+                                RedL.Fill = Brushes.Red;
+                                YellowL.Fill = Brushes.Black;
+                                GreenL.Fill = Brushes.Black;
+                                break;
+                            case MyEnum.RedAndYellow:
+                                timer.Interval = new TimeSpan(0, 0, 0, 1);
+                                RedL.Fill = Brushes.Red;
+                                YellowL.Fill = Brushes.Yellow;
+                                GreenL.Fill = Brushes.Black;
+                                break;
+                            case MyEnum.Green:
+                                timer.Interval = new TimeSpan(0, 0, 0, 3);
+                                RedL.Fill = Brushes.Black;
+                                YellowL.Fill = Brushes.Black;
+                                GreenL.Fill = Brushes.Green;
+                                break;
+                            case MyEnum.Yellow:
+                                timer.Interval = new TimeSpan(0, 0, 0, 1);
+                                RedL.Fill = Brushes.Black;
+                                YellowL.Fill = Brushes.Yellow;
+                                GreenL.Fill = Brushes.Black;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
